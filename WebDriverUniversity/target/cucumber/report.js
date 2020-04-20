28aca0f04763b1b85804a51a1e269fcc46330eee
Luis Marreiros <luishenriquelh1@hotmail.com>
2020-04-20 19:25:05 +0200

    Adding feature files and Step Implementations of Login and ContactUs feature

---
+++ WebDriverUniversity/target/cucumber/report.js
@@ -0,0 +1,425 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ContactUs.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Submit data to webdriveruniversity.com using contact us form",
+  "description": "A user should submit information via the contact us form",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "I access webdriveruniversity1",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on the contact1",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ContactUs.i_access_webdriveruniversity()"
+});
+formatter.result({
+  "duration": 15979391700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_click_on_the_login_contact_button()"
+});
+formatter.result({
+  "duration": 3659522101,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Submit information using the contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-the-contact-us-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter first name1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I enter last name1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I enter an email address1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I enter comments1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "I click on the submit button1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "confirmation",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUs.i_enter_first_name()"
+});
+formatter.result({
+  "duration": 4871626000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_last_name()"
+});
+formatter.result({
+  "duration": 218518999,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_an_email_address()"
+});
+formatter.result({
+  "duration": 407275999,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_comment()"
+});
+formatter.result({
+  "duration": 345115200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_click_on_the_submit_button()"
+});
+formatter.result({
+  "duration": 1102609700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.confirmation()"
+});
+formatter.result({
+  "duration": 150857501,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "I access webdriveruniversity1",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on the contact1",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ContactUs.i_access_webdriveruniversity()"
+});
+formatter.result({
+  "duration": 14009618200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_click_on_the_login_contact_button()"
+});
+formatter.result({
+  "duration": 3785101400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Submit invalid information using the contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-invalid-information-using-the-contact-us-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 20,
+  "name": "I enter first name1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I enter last name1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I enter an email address2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "I enter comments1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I click on the submit button1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "invalid confirmation",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUs.i_enter_first_name()"
+});
+formatter.result({
+  "duration": 5026031600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_last_name()"
+});
+formatter.result({
+  "duration": 299978201,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_an_email_address2()"
+});
+formatter.result({
+  "duration": 329020700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_enter_comment()"
+});
+formatter.result({
+  "duration": 605960299,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.i_click_on_the_submit_button()"
+});
+formatter.result({
+  "duration": 770691500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUs.confirmation2()"
+});
+formatter.result({
+  "duration": 132036000,
+  "status": "passed"
+});
+formatter.uri("Login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login to account at webdriveruniversity.com using login portal",
+  "description": "",
+  "id": "login-to-account-at-webdriveruniversity.com-using-login-portal",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "I access webdriveruniversity",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I click on the login portal button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter username",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginStep.i_access_webdriveruniversity()"
+});
+formatter.result({
+  "duration": 14071558700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_click_on_the_login_portal_button()"
+});
+formatter.result({
+  "duration": 731089800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_enter_username()"
+});
+formatter.result({
+  "duration": 5752220600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Login to account correct password",
+  "description": "",
+  "id": "login-to-account-at-webdriveruniversity.com-using-login-portal;login-to-account-correct-password",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter a \"webdriver123\" password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "I should be presented wit a successful validation alert",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "webdriver123",
+      "offset": 11
+    }
+  ],
+  "location": "LoginStep.i_enter_a_password(String)"
+});
+formatter.result({
+  "duration": 654856600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_click_on_the_login_button()"
+});
+formatter.result({
+  "duration": 232602800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_should_be_presented_wit_a_successful_validation_alert()"
+});
+formatter.result({
+  "duration": 111196301,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "I access webdriveruniversity",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I click on the login portal button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter username",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginStep.i_access_webdriveruniversity()"
+});
+formatter.result({
+  "duration": 17357046400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_click_on_the_login_portal_button()"
+});
+formatter.result({
+  "duration": 1058573600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_enter_username()"
+});
+formatter.result({
+  "duration": 5325083599,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "Login to account with a invalid password",
+  "description": "",
+  "id": "login-to-account-at-webdriveruniversity.com-using-login-portal;login-to-account-with-a-invalid-password",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 14,
+  "name": "I enter a \"invalid\" password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "I click on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "I should be presented with a unsuccessful validation alert",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "invalid",
+      "offset": 11
+    }
+  ],
+  "location": "LoginStep.i_enter_a_password(String)"
+});
+formatter.result({
+  "duration": 501643800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_click_on_the_login_button()"
+});
+formatter.result({
+  "duration": 669566600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.i_should_be_presented_wit_a_unsuccessful_validation_alert()"
+});
+formatter.result({
+  "duration": 305924101,
+  "status": "passed"
+});
+});
