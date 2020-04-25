923a8f8a1f0226246d3eaf028189e8e8ea234b41
Luis Marreiros <luishenriquelh1@hotmail.com>
2020-04-25 19:58:39 +0200

    Adding login feature using Scenario Outline

--- WebDriverUniversity/target/cucumber/report.js
+++ WebDriverUniversity/target/cucumber/report.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ContactUs.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ContactUsMultipleScenario.feature");
 formatter.feature({
   "line": 1,
   "name": "Submit data to webdriveruniversity.com using contact us form",
@@ -24,17 +24,17 @@ formatter.step({
   "keyword": "When "
 });
 formatter.match({
-  "location": "ContactUs.i_access_webdriveruniversity()"
+  "location": "ContactUsMultScenario.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "duration": 11397851501,
+  "duration": 12025085300,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_login_contact_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_login_contact_button()"
 });
 formatter.result({
-  "duration": 3226816600,
+  "duration": 3370563800,
   "status": "passed"
 });
 formatter.scenario({
@@ -92,45 +92,45 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ContactUs.i_enter_first_name()"
+  "location": "ContactUsMultScenario.i_enter_first_name()"
 });
 formatter.result({
-  "duration": 4240657401,
+  "duration": 4397663200,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_last_name()"
+  "location": "ContactUsMultScenario.i_enter_last_name()"
 });
 formatter.result({
-  "duration": 119777499,
+  "duration": 209597800,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_an_email_address()"
+  "location": "ContactUsMultScenario.i_enter_an_email_address()"
 });
 formatter.result({
-  "duration": 166847200,
+  "duration": 305372300,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_comment(DataTable)"
+  "location": "ContactUsMultScenario.i_enter_comment(DataTable)"
 });
 formatter.result({
-  "duration": 271727300,
+  "duration": 511847400,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_submit_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_submit_button()"
 });
 formatter.result({
-  "duration": 1058347899,
+  "duration": 1232208900,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.confirmation()"
+  "location": "ContactUsMultScenario.confirmation()"
 });
 formatter.result({
-  "duration": 49061699,
+  "duration": 472438700,
   "status": "passed"
 });
 formatter.background({
@@ -151,17 +151,17 @@ formatter.step({
   "keyword": "When "
 });
 formatter.match({
-  "location": "ContactUs.i_access_webdriveruniversity()"
+  "location": "ContactUsMultScenario.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "duration": 10580073500,
+  "duration": 10804181100,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_login_contact_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_login_contact_button()"
 });
 formatter.result({
-  "duration": 3356960600,
+  "duration": 3310568100,
   "status": "passed"
 });
 formatter.scenario({
@@ -203,45 +203,45 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ContactUs.i_enter_first_name()"
+  "location": "ContactUsMultScenario.i_enter_first_name()"
 });
 formatter.result({
-  "duration": 4233626100,
+  "duration": 4356332700,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_last_name()"
+  "location": "ContactUsMultScenario.i_enter_last_name()"
 });
 formatter.result({
-  "duration": 117873999,
+  "duration": 208895800,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_an_email_address2()"
+  "location": "ContactUsMultScenario.i_enter_an_email_address2()"
 });
 formatter.result({
-  "duration": 119625200,
+  "duration": 186061800,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_comment()"
+  "location": "ContactUsMultScenario.i_enter_comment()"
 });
 formatter.result({
-  "duration": 220690799,
+  "duration": 353019000,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_submit_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_submit_button()"
 });
 formatter.result({
-  "duration": 655413000,
+  "duration": 580487600,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.confirmation2()"
+  "location": "ContactUsMultScenario.confirmation2()"
 });
 formatter.result({
-  "duration": 100995899,
+  "duration": 397710900,
   "status": "passed"
 });
 formatter.background({
@@ -262,17 +262,17 @@ formatter.step({
   "keyword": "When "
 });
 formatter.match({
-  "location": "ContactUs.i_access_webdriveruniversity()"
+  "location": "ContactUsMultScenario.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "duration": 11012269200,
+  "duration": 10655650400,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_login_contact_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_login_contact_button()"
 });
 formatter.result({
-  "duration": 3180843100,
+  "duration": 3189779400,
   "status": "passed"
 });
 formatter.scenario({
@@ -314,48 +314,48 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ContactUs.i_enter_first_name()"
+  "location": "ContactUsMultScenario.i_enter_first_name()"
 });
 formatter.result({
-  "duration": 4250411601,
+  "duration": 4277024800,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_last_name()"
+  "location": "ContactUsMultScenario.i_enter_last_name()"
 });
 formatter.result({
-  "duration": 122316199,
+  "duration": 129390600,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_an_email_address()"
+  "location": "ContactUsMultScenario.i_enter_an_email_address()"
 });
 formatter.result({
-  "duration": 173391000,
+  "duration": 195118400,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_enter_comment()"
+  "location": "ContactUsMultScenario.i_enter_comment()"
 });
 formatter.result({
-  "duration": 235832501,
+  "duration": 244412400,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.i_click_on_the_reset_button()"
+  "location": "ContactUsMultScenario.i_click_on_the_reset_button()"
 });
 formatter.result({
-  "duration": 96320700,
+  "duration": 115162900,
   "status": "passed"
 });
 formatter.match({
-  "location": "ContactUs.empty_confirmartion()"
+  "location": "ContactUsMultScenario.empty_confirmartion()"
 });
 formatter.result({
-  "duration": 139912101,
+  "duration": 323095100,
   "status": "passed"
 });
-formatter.uri("Login.feature");
+formatter.uri("LoginBackground.feature");
 formatter.feature({
   "line": 1,
   "name": "Login to account at webdriveruniversity.com using login portal",
@@ -386,24 +386,24 @@ formatter.step({
   "keyword": "And "
 });
 formatter.match({
-  "location": "LoginStep.i_access_webdriveruniversity()"
+  "location": "LoginBackgroundStep.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "duration": 10538086399,
+  "duration": 10685444000,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_click_on_the_login_portal_button()"
+  "location": "LoginBackgroundStep.i_click_on_the_login_portal_button()"
 });
 formatter.result({
-  "duration": 383806499,
+  "duration": 389044700,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_enter_username()"
+  "location": "LoginBackgroundStep.i_enter_username()"
 });
 formatter.result({
-  "duration": 4234679401,
+  "duration": 4481175500,
   "status": "passed"
 });
 formatter.scenario({
@@ -436,24 +436,24 @@ formatter.match({
       "offset": 11
     }
   ],
-  "location": "LoginStep.i_enter_a_password(String)"
+  "location": "LoginBackgroundStep.i_enter_a_password(String)"
 });
 formatter.result({
-  "duration": 186689001,
+  "duration": 396001800,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_click_on_the_login_button()"
+  "location": "LoginBackgroundStep.i_click_on_the_login_button()"
 });
 formatter.result({
-  "duration": 116610899,
+  "duration": 241422800,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_should_be_presented_wit_a_successful_validation_alert()"
+  "location": "LoginBackgroundStep.i_should_be_presented_wit_a_successful_validation_alert()"
 });
 formatter.result({
-  "duration": 7499700,
+  "duration": 367299300,
   "status": "passed"
 });
 formatter.background({
@@ -479,24 +479,24 @@ formatter.step({
   "keyword": "And "
 });
 formatter.match({
-  "location": "LoginStep.i_access_webdriveruniversity()"
+  "location": "LoginBackgroundStep.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "duration": 10902763500,
+  "duration": 10548647300,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_click_on_the_login_portal_button()"
+  "location": "LoginBackgroundStep.i_click_on_the_login_portal_button()"
 });
 formatter.result({
-  "duration": 286321499,
+  "duration": 238066100,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_enter_username()"
+  "location": "LoginBackgroundStep.i_enter_username()"
 });
 formatter.result({
-  "duration": 4231358801,
+  "duration": 4412739600,
   "status": "passed"
 });
 formatter.scenario({
@@ -529,24 +529,341 @@ formatter.match({
       "offset": 11
     }
   ],
-  "location": "LoginStep.i_enter_a_password(String)"
+  "location": "LoginBackgroundStep.i_enter_a_password(String)"
 });
 formatter.result({
-  "duration": 157492500,
+  "duration": 308437300,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_click_on_the_login_button()"
+  "location": "LoginBackgroundStep.i_click_on_the_login_button()"
 });
 formatter.result({
-  "duration": 107582100,
+  "duration": 192907200,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStep.i_should_be_presented_wit_a_unsuccessful_validation_alert()"
+  "location": "LoginBackgroundStep.i_should_be_presented_wit_a_unsuccessful_validation_alert()"
 });
 formatter.result({
-  "duration": 6430200,
+  "duration": 336625100,
+  "status": "passed"
+});
+formatter.uri("LoginScenarioOutline.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login to Account",
+  "description": "",
+  "id": "login-to-account",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Longin to account with correct credentials",
+  "description": "",
+  "id": "login-to-account;longin-to-account-with-correct-credentials",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user navigates to \"\u003curl\u003e\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user clicks on the login portal button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters the \"\u003cusername\u003e\" username",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters the \"\u003cpassword\u003e\" password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "the user should be presented with the following prompt alert \"\u003cmessage\u003e\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 12,
+  "name": "",
+  "description": "",
+  "id": "login-to-account;longin-to-account-with-correct-credentials;",
+  "rows": [
+    {
+      "cells": [
+        "url",
+        "username",
+        "password",
+        "message"
+      ],
+      "line": 13,
+      "id": "login-to-account;longin-to-account-with-correct-credentials;;1"
+    },
+    {
+      "cells": [
+        "http://www.webdriveruniversity.com",
+        "user1",
+        "pass1",
+        "validation failed"
+      ],
+      "line": 14,
+      "id": "login-to-account;longin-to-account-with-correct-credentials;;2"
+    },
+    {
+      "cells": [
+        "http://www.webdriveruniversity.com",
+        "webdriver",
+        "webdriver123",
+        "validation succeeded"
+      ],
+      "line": 15,
+      "id": "login-to-account;longin-to-account-with-correct-credentials;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "Longin to account with correct credentials",
+  "description": "",
+  "id": "login-to-account;longin-to-account-with-correct-credentials;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user navigates to \"http://www.webdriveruniversity.com\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user clicks on the login portal button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters the \"user1\" username",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters the \"pass1\" password",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "the user should be presented with the following prompt alert \"validation failed\"",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://www.webdriveruniversity.com",
+      "offset": 19
+    }
+  ],
+  "location": "LoginScenarioOutline.user_navigates_to(String)"
+});
+formatter.result({
+  "duration": 11055284900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginScenarioOutline.user_clicks_on_the_login_portal_button()"
+});
+formatter.result({
+  "duration": 566971300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "user1",
+      "offset": 17
+    }
+  ],
+  "location": "LoginScenarioOutline.user_enters_the_username(String)"
+});
+formatter.result({
+  "duration": 4404134600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "pass1",
+      "offset": 17
+    }
+  ],
+  "location": "LoginScenarioOutline.user_enter_the_password(String)"
+});
+formatter.result({
+  "duration": 264856300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginScenarioOutline.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 138139200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "validation failed",
+      "offset": 62
+    }
+  ],
+  "location": "LoginScenarioOutline.the_user_should_be_presented_with_the_following_prompt_alert(String)"
+});
+formatter.result({
+  "duration": 262533100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Longin to account with correct credentials",
+  "description": "",
+  "id": "login-to-account;longin-to-account-with-correct-credentials;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user navigates to \"http://www.webdriveruniversity.com\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user clicks on the login portal button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters the \"webdriver\" username",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters the \"webdriver123\" password",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "the user should be presented with the following prompt alert \"validation succeeded\"",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://www.webdriveruniversity.com",
+      "offset": 19
+    }
+  ],
+  "location": "LoginScenarioOutline.user_navigates_to(String)"
+});
+formatter.result({
+  "duration": 10763738300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginScenarioOutline.user_clicks_on_the_login_portal_button()"
+});
+formatter.result({
+  "duration": 201061400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "webdriver",
+      "offset": 17
+    }
+  ],
+  "location": "LoginScenarioOutline.user_enters_the_username(String)"
+});
+formatter.result({
+  "duration": 4342704600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "webdriver123",
+      "offset": 17
+    }
+  ],
+  "location": "LoginScenarioOutline.user_enter_the_password(String)"
+});
+formatter.result({
+  "duration": 229721100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginScenarioOutline.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 90345600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "validation succeeded",
+      "offset": 62
+    }
+  ],
+  "location": "LoginScenarioOutline.the_user_should_be_presented_with_the_following_prompt_alert(String)"
+});
+formatter.result({
+  "duration": 204601800,
   "status": "passed"
 });
 });
